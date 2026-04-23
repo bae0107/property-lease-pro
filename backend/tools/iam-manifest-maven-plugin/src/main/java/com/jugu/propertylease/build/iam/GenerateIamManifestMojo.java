cfe9d66a6179022ede7d4effe2cf65c6eb950435
peiyong-hp <peiyong0107@sina.com>
2026-04-23 22:19:37 +0800

    1.fix compile

--- backend/tools/iam-manifest-maven-plugin/src/main/java/com/jugu/propertylease/build/iam/GenerateIamManifestMojo.java
+++ backend/tools/iam-manifest-maven-plugin/src/main/java/com/jugu/propertylease/build/iam/GenerateIamManifestMojo.java
@@ -2,6 +2,7 @@ package com.jugu.propertylease.build.iam;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
